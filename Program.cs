5cc0f52bd55f955389189a3a439345a4478a5638
eduardo-carrilho <dudu_carrilho@hotmail.com>
2025-01-15 12:32:46 -0300

    commit do desafio final

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "123123123", memoria: 64);
+
+nokia.Ligar();
+nokia.InstalarAplicativo("Telegram");
+
+Smartphone iphone = new Iphone(numero: "321321", modelo: "Modelo 2", imei: "321321321", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
